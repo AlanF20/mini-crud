9e7c811f918d5009da2bbd9fbac28f09f16be988
Alan Flores <alanif@hotmail.com>
2023-01-02 04:59:27 -0600

    refactor the component to work properly(mb)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
-const features = ['Crear usuario', 'Ver usuarios']
-const listOfFeatures = features.map((feature, index) => {
-  return (
-    <li key={index} className='header__nav-ul-li'>
-      <a href='#'>{feature}</a>
-    </li>
-  )
-})
-
-export function Header () {
+export function Header() {
+  const features = ['Crear usuario', 'Ver usuarios']
+  const listOfFeatures = features.map((feature, index) => {
+    return (
+      <li key={index} className='header__nav-ul-li'>
+        <a className="header__nav-ul-li-link" href='#'>{feature}</a>
+      </li>
+    )
+  })
   return (
     <header className='header'>
       <nav className='header__nav'>
