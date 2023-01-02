00e4e9c03b0402ae04f4bce1778b5eb9674d9c63
Alan Flores <alanif@hotmail.com>
2023-01-02 08:09:47 -0600

    adding the alert message for successfull data send

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
 import { useState } from "react"
-
+import AlertMessage from "./Components/AlertMessage.jsx"
 
 const App = () => {
   const [userData, setUserData] = useState({})
+  const [isSuccess, setSuccess] = useState()
   const handleOnChange = (e) => {
     setUserData({
       ...userData,
       [e.target.name]: e.target.value
     })
   }
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    const sendUser = await fetch("http://localhost:3000/usuarios", {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    })
-    const response = await sendUser.json()
+    try {
+      const sendUser = await fetch("http://localhost:3000/usuarios", {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+      setSuccess(true)
+    }catch(error){
+      console.error(error)
+      setSuccess(false)
+    }
   }
   return (
-    <form method="post" onSubmit={handleSubmit} className="form">
-      <label htmlFor="user">
-        Nombre: <input type='text' id='user' name="user" value={userData.name} placeholder='Inserte su name' onChange={handleOnChange} />
-      </label>
-      <label htmlFor="userEmail">
-        Email: <input type='email' id='userEmail' name="userEmail" value={userData.email} placeholder='Inserte su email' onChange={handleOnChange} />
-      </label>
-      <label htmlFor="userPassword">
-        Contra <input type='password' id='userPassword' name="userPassword" value={userData.password} placeholder='Inserte su contra' onChange={handleOnChange} />
-      </label>
-      <input className="form__submit" type='submit' value='Enviar mi informacion' />
-    </form>
+    <>
+      <form method="post" onSubmit={handleSubmit} className="form">
+        <label htmlFor="user">
+          Nombre: <input type='text' id='user' name="user" value={userData.name} placeholder='Inserte su name' onChange={handleOnChange} />
+        </label>
+        <label htmlFor="userEmail">
+          Email: <input type='email' id='userEmail' name="userEmail" value={userData.email} placeholder='Inserte su email' onChange={handleOnChange} />
+        </label>
+        <label htmlFor="userPassword">
+          Contra <input type='password' id='userPassword' name="userPassword" value={userData.password} placeholder='Inserte su contra' onChange={handleOnChange} />
+        </label>
+        <input className="form__submit" type='submit' value='Enviar mi informacion' />
+      </form>
+      <AlertMessage status={isSuccess} />
+    </>
   );
 }
 
