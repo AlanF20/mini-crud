ceae98a106d0e196126ee399591050d70cf8e59a
Alan Flores <alanif@hotmail.com>
2023-01-09 00:37:58 -0600

    adding the useEffect hook to fetch the users into the /Ver path

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react'
 import Header from './Header'
 
 export default function Users () {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/usuarios')
+      .then(response => response.json())
+      .then(user => setUsers(user))
+  }, [])
   return (
     <>
       <Header />
-      <h1>Hola mundo</h1>
+      <h1>Usuarios creados hasta el momento</h1>
+      <ul>
+        {users.map(user => {
+          return (
+            <li key={user.user_id}>
+              <p>{user.user_name}</p>
+              <p>{user.user_email}</p>
+            </li>
+          )
+        })}
+      </ul>
+
     </>
   )
 }
