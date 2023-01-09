5fd9b65fa22fe88b8189c761aa2cec1c357e73ca
Alan Flores <alanif@hotmail.com>
2023-01-08 20:16:19 -0600

    adding react router dom and the layout to use

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,14 @@
 export function Header () {
-  const features = ['Crear usuario', 'Ver usuarios']
-  const listOfFeatures = features.map((feature, index) => {
-    return (
-      <li key={index} className='header__nav-ul-li'>
-        <a className='header__nav-ul-li-link' href='#'>{feature}</a>
-      </li>
-    )
-  })
   return (
     <header className='header'>
       <nav className='header__nav'>
         <ul className='header__nav-ul'>
-          {listOfFeatures}
+          <li className='header__nav-ul-li'>
+            <a className='header__nav-ul-li-link' href='#'>Crear usuario</a>
+          </li>
+          <li className='header__nav-ul-li'>
+            <a className='header__nav-ul-li-link' href='#'>Ver usuarios</a>
+          </li>
         </ul>
       </nav>
     </header>
