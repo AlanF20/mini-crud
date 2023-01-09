13ae28990000c9c30437d6787cd66fcd980c20d5
Alan Flores <alanif@hotmail.com>
2023-01-09 14:20:38 -0600

    refactor: adding styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ export function App () {
   return (
     <>
       <Header />
-      <main>
+      <main className='main'>
         <h1>Welcome to my mini crud application, I'm Alan Flores and this is what I made</h1>
         <p>This is just an app to create, delete, search and update information from a user, using tecnologies like react, react router dom, and Node.js on the backend with Express</p>
       </main>
